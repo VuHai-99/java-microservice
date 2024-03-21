37a7f64f0ebf85bf1f289eb5522d96ec766acf1c
Henry <henry@Henrys-MacBook-Pro-2.local>
2024-03-21 22:13:48 +0700

    feat: create entity & value object of OrderDomainCore

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/CustomerId.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/CustomerId.java
@@ -2,8 +2,8 @@ package com.food.ordering.system.domain.valueobject;
 
 import java.util.UUID;
 
-public class CustomertId extends BaseId<UUID>{
-    public CustomertId(UUID value) {
+public class CustomerId extends BaseId<UUID>{
+    public CustomerId(UUID value) {
         super(value);
     }
 }
