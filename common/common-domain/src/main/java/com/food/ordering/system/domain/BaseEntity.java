7cf75663415e29dc31cbc0637b3f1fb473098489
Henry <henry@Henrys-MacBook-Pro-2.local>
2024-03-21 21:27:03 +0700

    feat: add basic ValueObject & Event

--- common/common-domain/src/main/java/com/food/ordering/system/domain/BaseEntity.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.domain.entity;
+package com.food.ordering.system.domain;
 
 import java.util.Objects;
 
---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/CustomertId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.util.UUID;
+
+public class CustomertId extends BaseId<UUID>{
+    public CustomertId(UUID value) {
+        super(value);
+    }
+}
